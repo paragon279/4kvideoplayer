5d1230ba1d04966d2c72dfd3912afa57084d563a
yinyangwarrior0928 <he.li909999@gmail.com>
2023-10-26 01:25:25 +0900

    add:

---
+++ player-windows/testplayer/resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by player.rc
+//
+#define IDD_PLAYER_DIALOG               102
+#define IDR_MAINFRAME                   128
+#define IDR_ACCELERATOR1                129
+#define ID_OPEN_FILE                    32771
+#define ID_AUDIO_STREAM                 32772
+#define ID_VIDEO_STREAM                 32773
+#define ID_VIDEO_MODE                   32774
+#define ID_EFFECT_MODE                  32775
+#define ID_VRENDER_TYPE                 32776
+#define ID_TAKE_SNAPSHOT                32777
+#define ID_STEP_FORWARD                 32778
+#define ID_STEP_BACKWARD                32779
+#define ID_PLAY_SPEED_DEC               32780
+#define ID_PLAY_SPEED_INC               32781
+#define ID_PLAY_SPEED_TYPE              32782
+#define ID_VDEVD3D_ROTATE               32783
+#define ID_RECORD_VIDEO                 32784
+#define ID_DEFINITION_EVAL              32787
+#define ID_WINFIT_VIDEOSIZE             32789
+#define ID_ZOOM_RESTORE                 32791
+#define ID_LIVEDESK_MODE                32794
+#define ID_SHOW_DATARATE                32796
+#define ID_YOLO_DETECT                  32798
+#define ID_VOLUME_UP                    32801
+#define ID_VOLUME_DOWN                  32802
+#define ID_SINGLE_PLAYER                32804
+#define ID_REAR_PLAYER                  32805
+#define ID_STOP                         32807
+#define ID_GET_FPS                      32809
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32811
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
